5e99dcf9756d27fd4666179db45000fe413f3423
Eason <chenzscd@163.com>
2020-07-29 19:49:08 +0800

    [Company API][Eason] test: implement should_return_all_companies_when_get_all_company_given_no_parameter test

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+
+import java.util.List;
+
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> findAll() {
+        return companyRepository.findAll();
+    }
+}
